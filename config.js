19f7faa62d44e1003485c6d79b446a30d5976049
andre104ei <145247006+andre104ei@users.noreply.github.com>
2023-09-24 18:43:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 
 global.owner = [
-  ['390371717717', 'nobody|'],
-  ['393518398856', 'nobody|'],
-  ['393755775755', 'nobody|'],
-  ['393755575577', 'nobody|'],
-  ['33760536110', 'nobody|'],
+  ['3109561855', 'nobody|'],
+  ['3109561855', 'nobody|'],
+  ['3109561855', 'nobody|'],
+  ['3109561855', 'nobody|'],
+  ['3109561855', 'nobody|'],
 ]
-global.suittag = ['390371717717'] 
-global.prems = ['390371717717'] 
-global.reportes_solicitudes = ['390371717717']
+global.suittag = ['3109561855'] 
+global.prems = ['3109561855'] 
+global.reportes_solicitudes = ['3109561855']
 
 global.packname = '(☞ﾟ∀ﾟ)☞'
 global.author = '★ცơɬῳɧąıɬą★'
